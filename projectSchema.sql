0bd87e15a73166854c4528c646b54655e9e98d83
yousef ellban <155241152+yusefellban@users.noreply.github.com>
2026-01-01 22:42:00 +0200

    update schema Implementation

--- projectSchema.sql
+++ projectSchema.sql
@@ -3,10 +3,10 @@ USE GameSystem;
 
 -- 1. Create the Player table
 CREATE TABLE Player (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     user_name VARCHAR(50) NOT NULL UNIQUE,
     hashed_pass VARCHAR(255) NOT NULL,
-    char_no VARCHAR(10) default "dragon",
+    char_no VARCHAR(8) default "dragon",
     score INT DEFAULT 0,
     isActive BOOLEAN DEFAULT FALSE,
     isAvailable BOOLEAN DEFAULT FALSE
@@ -14,9 +14,9 @@ CREATE TABLE Player (
 
 -- 2. Create the Game table
 CREATE TABLE Game (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status INT COMMENT '1: player1 won, 2: player1 won, 3: draw',
+    status INT COMMENT '1: player1 won, 2: player2 won, 3: draw',
     player1ID INT NOT NULL,
     player2ID INT NOT NULL,
     -- Establishing Foreign Key relationships
